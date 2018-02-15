be53ee26ef0ca7f98ffee0549650ad9c84ef262b
jaro <jarohabiger@googlemail.com>
2018-02-15 14:24:51 +0100

    cleanup

--- src/FragmentShaderRenderer.js
+++ src/FragmentShaderRenderer.js
@@ -3,129 +3,108 @@
  */
 "use strict";
 
-module.exports = class FragmentShaderRenderer {
-    /**
-     * Initializes a new fragment shader renderer for the given canvas
-     * @param canvas the canvas for rendering
-     * @param options an object with the fields {size: {x: number, y: number}}
-     */
-    constructor(canvas, options) {
-        this.options = options;
+import React from "react";
 
-        this.canvas = canvas;
-        this.gl = canvas.getContext("webgl2");
+export default class FragmentShaderRenderer extends React.Component {
+    render() {
+        // everything is manages by the lifecycle functions.
+        return <canvas ref={c => { this.canvas = c; }}/>;
+    }
+
+    componentDidMount() {
+        this.compilerCallback = this.props.compilerCallback ? this.props.compilerCallback : () => null;
+
+        this.gl = this.canvas.getContext("webgl2");
+        if(!this.gl) {
+            this.compilerCallback("Error. Webgl2 is not supported in your browser");
+            return;
+        }
         let gl = this.gl;
         this.program = gl.createProgram();
 
+        // initialize with dummy shader
+        this._setShader('FRAGMENT_SHADER',
+            `#version 300 es
+            precision mediump float;
 
-        // set the data for drawing the base sqauare from 2 triangles
-        this.vertex_shader = this._createShader(gl.VERTEX_SHADER,
+            out vec4 fragColor;
+
+            void main() {
+                fragColor = vec4( .0 );
+            }`
+        );
+
+        // set the data for drawing the base square from 2 triangles with pass through vertex shader
+        this._setShader('VERTEX_SHADER',
             `#version 300 es
             in vec2 position;
             
             void main() {
                 gl_Position = vec4( position.x, position.y, 1.0, 1.0 );
-            }`);
-        gl.attachShader(this.program, this.vertex_shader);
+            }`
+        );
 
-        this.fragment_shader = this._createShader(gl.FRAGMENT_SHADER,
-            `#version 300 es
-            precision mediump float;
-            
-            out vec4 fragColor;
-            
-            void main() {
-                fragColor = vec4( 0. );
-            }`);
-        gl.attachShader(this.program, this.fragment_shader);
-
-        gl.linkProgram(this.program);
-
-        this.base_triangle_points = [
-            -1, -1,
-            -1, 1,
-            1, 1,
-            -1, -1,
-            1, 1,
-            1, -1
-        ];
-        let positionAttributeLocation = this._setVertexAttribute("position", this.base_triangle_points);
+        let positionAttributeLocation = this._setVertexAttribute("position", [
+            -1,  -1,
+            -1,   1,
+            1 ,   1,
+            -1,  -1,
+            1 ,   1,
+            1 ,  -1
+        ]);
         gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
 
-        // set the canvas and the viewport size
-        gl.viewport(0, 0, this.options.size.x, this.options.size.y);
-        gl.canvas.width = this.options.size.x;
-        gl.canvas.height = this.options.size.y;
+        // finally do the normal rendering steps
+        this.componentDidUpdate(this.props);
     }
 
-    /**
-     * renders the current shader. this method should be called after every e
-     */
-    render() {
+    componentDidUpdate(prevProps) {
         let gl = this.gl;
 
+        // update the fragmentshader if needed
+        this._setShader('FRAGMENT_SHADER', prevProps.shader);
+
+
+        // set the canvas and the viewport size
+        gl.viewport(0, 0, this.props.width, this.props.height);
+        gl.canvas.width = this.props.width;
+        gl.canvas.height = this.props.height;
+
+        // really do the rendering
         gl.clearColor(0, 0, 0, 0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.useProgram(this.program);
         gl.drawArrays(gl.TRIANGLES, 0, 6);
     }
 
-    /**
-     * sets the fragment shader code, that should be rendered to the canvas
-     * @param code the glsl code of the fragment shader
-     */
-    setFragmentShader(code) {
+// low level util functions
+    _setShader(type, shaderCode) {
         let gl = this.gl;
+        let shaderCodeRef = type.toLowerCase() + '_code';
+        let shaderRef = type.toLowerCase();
+        if (this[shaderCodeRef] !== shaderCode) {
+            try {
+                let newShader = this._createShader(type, shaderCode);
+                try {
+                    gl.detachShader(this.program, this[shaderRef]);
+                    gl.deleteShader(this[shaderRef])
+                } catch (ignore) {}
+                this[shaderRef] = newShader;
 
-        this.fragment_shader_code = code;
+                gl.attachShader(this.program, this[shaderRef]);
+                gl.linkProgram(this.program);
 
-        gl.detachShader(this.program, this.fragment_shader);
-        this.fragment_shader = this._createShader(gl.FRAGMENT_SHADER, this.fragment_shader_code);
-        gl.attachShader(this.program, this.fragment_shader);
-
-        gl.linkProgram(this.program);
-        let success = gl.getProgramParameter(this.program, gl.LINK_STATUS);
-        if (!success) {
-            console.log(this.gl.getProgramInfoLog(this.program));
+                this[shaderCodeRef] = shaderCode;
+            } catch (compilerError) {
+                this.compilerCallback(compilerError)
+            }
         }
     }
-
-    /**
-     * sets a texture for the FragmentShaderRenderer
-     * @param name the name of the sampler2D
-     * @param data the data of the texture
-     * @param size the size of the texture in the form {x: number, y: number}
-     * @param options the options for the texture sampler
-     */
-    setTexture2D(name, data, size, options) {
-        let gl = this.gl;
-        let texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        Object.keys(options).forEach(k => {
-            gl.texParameteri(gl.TEXTURE_2D, this._getGlConst(k), this._getGlConst(options[k]));
-        });
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.R32F, size.x, size.y, 0, gl.RED, gl.FLOAT, data);
-    }
-
-    /**
-     * sets a uniform for the FragmentShaderRenderer
-     * @param name the mane of the uniform
-     * @param type the type of the uniform ie. uniform1f
-     * @param data the data that should be in the uniform
-     */
-    setUniform(name, type, data) {
-        gl.useProgram(this.program);
-        let loc = gl.getUniformLocation(program, name);
-        this._getGlConst(type)(loc, data);
-    }
-
-
-
     _createShader(type, source) {
         let gl = this.gl;
 
-        let shader = gl.createShader(type);
+        let shader = gl.createShader(this._getGlConst(type));
         gl.shaderSource(shader, source);
         gl.compileShader(shader);
         let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
@@ -133,11 +112,11 @@ module.exports = class FragmentShaderRenderer {
         if (success) {
             return shader;
         } else {
+            let error = gl.getShaderInfoLog(shader);
             gl.deleteShader(shader);
-            throw {type: "compilation error", payload: gl.getShaderInfoLog(shader)};
+            throw error;
         }
     }
-
     _setVertexAttribute(name, data) {
         let gl = this.gl;
 
@@ -149,8 +128,7 @@ module.exports = class FragmentShaderRenderer {
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         return attributeLocation;
     }
-
     _getGlConst(name) {
         return this.gl[name];
     }
-};
+}
--- src/index.js
+++ src/index.js
@@ -4,117 +4,26 @@
  */
 "use strict";
 
-/*
-const shader_preprocessor = require('./shader_preprocessor');
-
-const panzoom = require('../lib/panzoom/panzoom.min.js');
-const ace = require('../lib/ace/lib/ace/ace');
-
-
-const initialModel = {
-    globalError: null,
-
-    fragment_shader: null,
-    compiler_errors: '',
-
-    inputs: {},
-};
-
-async function update(model, e) {
-    if(e === 'initial') {
-        return 'load_shader';
-    } else if (e === 'load_shader') {
-        model.fragment_shader = await loaders.loadText("examples/shaders/black_white.glsl");
-        return 'load_resources';
-    } else if (e === 'load_resources') {
-        model.inputs['raw_image'] = await loaders.loadRaw16("examples/images/human.raw16");
-    } else if (typeof e === error) {
-        model.globalError = e.me
-    }
-}
-
-async function view(model) {
-    if(!model.fragment_shader) {
-        return `
-            <div id="loading">
-                LOADING SHADER...
-            </div>
-        `;
-    } else if (Object.keys(shader_preprocessor.getRequiredInputs(model.fragment_shader)).filter(x => !model.inputs[x]).length > 0) {
-        return `
-            <div id="loading">
-                LOADING RESOURCES...
-            </div>
-        `;
-    } else {
-        return `
-            <main>
-                <canvas id="canvas"></canvas>
-            </main>
-        
-            <aside>
-                <h1>### BATIC </h1>
-                <a href="https://github.com/anuejn/batic" target="_blank" style="position: absolute; right: 20px; top: 40px;">(WHATS THIS?!)</a>
-        
-                <div id="controls">
-                    ${generateControls(model.fragment_shader)}
-                </div>
-                <div id="code"></div>
-                <pre id="errors">
-                    ${model.compiler_errors}
-                </pre>
-            </aside>
-        `;
-    }
-}
-
-function generateControls(fragment_shader) {
-    const needed_inputs = shader_preprocessor.getRequiredInputs(fragment_shader);
-    return Object.keys(needed_inputs).map(name => {
-        try {
-            let input = inputs.filter(i => i.type.indexOf(needed_inputs[name]) !== -1)[0];
-            console.log(input.render(name));
-            return input.render(name);
-        } catch (e) {
-            return `<div class="error">no input for uniform ${name} with type ${needed_inputs[name]}`
-        }
-    }).reduce((a, b) => a + b);
-}
-
-async function lower(rendered) {
-    const container = document.querySelector("body");
-    if(rendered !== container.innerHTML) {
-        // set the html
-        container.innerHTML = rendered;
-
-
-        // setup the code editor
-        window.code = ace.edit("code");
-        window.code.session.setMode("ace/mode/glsl");
-        ace.require("ace/ext/language_tools");
-        window.code.setOptions({
-            enableBasicAutocompletion: true,
-            enableSnippets: true,
-            enableLiveAutocompletion: true
-        });
-
-        // setup panzoom
-        let scene = document.getElementById("canvas");
-        panzoom(scene, {
-            smoothScroll: true
-        })
-
-    }
-}
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import FragmentShaderRenderer from './FragmentShaderRenderer';
+
 
 const App = () => (
     <div className="App">
         <h1 className="App-Title">Hello Parcel x React</h1>
+        <FragmentShaderRenderer width="100" height="100" compilerCallback={console.warn} shader={
+            `#version 300 es
+            precision mediump float;
+
+            out vec4 fragColor;
+
+            void main() {
+                fragColor = vec4( 1. );
+            }`
+        }/>
     </div>
 );
 
